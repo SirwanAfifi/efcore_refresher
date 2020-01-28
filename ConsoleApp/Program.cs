2b3c051f6abc304131302070da707d159732f15b
sirwan <sir1afifi@gmail.com>
2020-01-28 23:28:42 +0330

    Added Console Code (just for demo)

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Linq;
+using Data;
+using Domain;
 
 namespace ConsoleApp
 {
     class Program
     {
+        private static SaumraiContext context = new SaumraiContext();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            context.Database.EnsureCreated();
+            GetSamuraies("Before Add:");
+            AddSamuraie();
+            GetSamuraies("After Add:");
+            Console.Write("Press any key...");
+            Console.ReadKey();
+        }
+
+        private static void AddSamuraie()
+        {
+            var samuraie = new Samurai { Name = "Sampson " };
+            context.Samuraies.Add(samuraie);
+            context.SaveChanges();
+        }
+
+        private static void GetSamuraies(string text)
+        {
+            var samuraies = context.Samuraies.ToList();
+            Console.Write($"${text}: Samurai count is ${samuraies.Count}");
+            foreach (var samuraie in samuraies)
+            {
+                Console.WriteLine(samuraie.Name);
+            }
         }
     }
 }
