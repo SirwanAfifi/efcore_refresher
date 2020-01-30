2d5708035742404b27700b9a106d31c46595f3b8
sirwan <sir1afifi@gmail.com>
2020-01-30 10:22:46 +0330

    Add migrations

--- Data/Migrations/20200130065148_AddRelationships.Designer.cs
+++ Data/Migrations/20200130065148_AddRelationships.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Data.Migrations
 {
     [DbContext(typeof(SaumraiContext))]
-    partial class SaumraiContextModelSnapshot : ModelSnapshot
+    [Migration("20200130065148_AddRelationships")]
+    partial class AddRelationships
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
